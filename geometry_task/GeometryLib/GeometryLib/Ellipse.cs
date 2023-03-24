861edd5f100fce89f5be32950edffcccfd32354c
CyberWolf <belozercev.i.24@gmail.com>
2023-03-24 16:53:06 +0300

    add wrong radius exception

--- geometry_task/GeometryLib/GeometryLib/Ellipse.cs
+++ geometry_task/GeometryLib/GeometryLib/Ellipse.cs
@@ -9,13 +9,37 @@ namespace GeometryLib
 
         public Ellipse(double radiusA, double radiusB)
         {
+            if (!ValidateRadius(radiusA) && !ValidateRadius(radiusB))
+            {
+                SmallRadius = 0d;
+                BigRadius = 0d;
+                return;
+            }
+
             SmallRadius = Math.Min(radiusA, radiusB);
             BigRadius = Math.Max(radiusA, radiusB);
+        }
+
+        // if radius is valid return true.
+        private bool ValidateRadius(double radius)
+        {
+            if (radius == 0d)
+            {
+                throw new WrongRadiusException(radius);
+            }
+
+            return true;
         }
 
         public double GetArea()
         {
             return Math.PI * SmallRadius * BigRadius;
         }
+    }
+
+    [Serializable]
+    public class WrongRadiusException : Exception
+    {
+        public WrongRadiusException(double radius) : base($"{typeof(WrongRadiusException)}\n Can\'t hold the {radius}") { }
     }
 }
